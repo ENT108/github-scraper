1faf3ad2e33fcca7622fb973c50c964893dd19a3
Adam Klukowski <a.klukowski@uruzlabs.com>
2020-07-28 23:42:43 +0200

    Adding user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import UserContextProvider from './contexts/user'
+import { Search } from './components/Search'
+import { UserInfo } from './components/UserInfo'
+import { TopRepos } from './components/TopRepos'
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <UserContextProvider>
+      <Search />
+      <UserInfo />
+      <TopRepos />
+    </UserContextProvider>
+  )
 }
 
-export default App;
+export default App
