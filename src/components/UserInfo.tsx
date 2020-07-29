ee793f5e2392067e4102fe9cb2cef1eab2794241
Adam Klukowski <a.klukowski@uruzlabs.com>
2020-07-30 00:37:15 +0200

    styling user info for mobile

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,33 +1,35 @@
 import React, { FC, useContext } from 'react'
-import { UserContext, UserContextState } from '../contexts/user'
+import { UserContext } from '../contexts/user'
+import { UserContextState } from '../contexts/user.type'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/esm/Container'
 
 export const UserInfo: FC = () => {
   const { getState } = useContext(UserContext)
-  const userInfo: UserContextState = getState()
+  const { details }: UserContextState = getState()
 
   return (
-    <>
-      {userInfo.login.length ?
-        <>
-          <Row>
-            <Col>
-              <Image src={userInfo.avatar_url} rounded fluid thumbnail />
-            </Col>
-            <Col>
-              {userInfo.login}
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              {userInfo.bio}
-            </Col>
-          </Row>
-        </>
-        : <><h3>None found</h3></>
-      }
-    </>
+    <section className="user">
+      <Container>
+        {details?.login?.length ?
+          <>
+            <Row>
+              <Col>
+                <Image src={details.avatar_url} fluid />
+                <span className="name">{details.name || details.login}</span>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <span className="bio">{details.bio}</span>
+              </Col>
+            </Row>
+          </>
+          : <><h3>None found</h3></>
+        }
+      </Container>
+    </section>
   )
 }
