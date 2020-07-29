eb23437b576dfa3c32573ca086a3a4f974bc8d25
Adam Klukowski <a.klukowski@uruzlabs.com>
2020-07-30 00:38:28 +0200

    main layout update

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import React, { FC } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import UserContextProvider from './contexts/user'
-import { Search } from './components/Search'
+import { Search } from './components/Search/Search'
 import { UserInfo } from './components/UserInfo'
-import { TopRepos } from './components/TopRepos'
-import Container from 'react-bootstrap/Container'
+import { TopRepos } from './components/TopRepos/TopRepos'
 
 const App: FC = () => {
   return (
     <UserContextProvider>
-      <Container>
-        <Search />
-        <UserInfo />
-        <TopRepos />
-      </Container>
+      <Search />
+      <UserInfo />
+      <TopRepos />
     </UserContextProvider>
   )
 }
